e79193c108e92e6a598d3e749de8f7e761d4bc65
BJH <imganchi@gmail.com>
2025-05-30 14:46:21 +0900

    fix

--- src/main/java/org/example/verificationservice/event/VerificationRequestMessage.java
+++ src/main/java/org/example/verificationservice/event/VerificationRequestMessage.java
@@ -3,5 +3,5 @@ package org.example.verificationservice.event;
 public record VerificationRequestMessage(
         Long purchaseId,
         String impUid,
-        int amount // 결제된 금액
+        int paidAmount // 결제된 금액
 ) {}
